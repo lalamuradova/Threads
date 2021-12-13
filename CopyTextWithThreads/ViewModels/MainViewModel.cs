5dc9d9de36d8b75cdcfba8175b9518ea67e923d0
Lala Muradova <Lalamuradova2017@gmail.com>
2021-12-13 16:14:05 +0400

    done

--- CopyTextWithThreads/ViewModels/MainViewModel.cs
+++ CopyTextWithThreads/ViewModels/MainViewModel.cs
@@ -5,7 +5,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace CopyTextWithThreads.ViewModels
 {
@@ -20,14 +25,19 @@ namespace CopyTextWithThreads.ViewModels
         public string Filename2 { get; set; }
         public string Data { get; set; }
         public string CopyData { get; set; } = string.Empty;
-        public int Count { get; set; }
+        public int Count { get; set; } = 0;
         public OpenFileDialog Open { get; set; } = new OpenFileDialog();
+        public Thread thread1;
 
-
+        public DispatcherTimer DT { get; set; } = new DispatcherTimer();
+        public bool isSuspend { get; set; } = false;
+        public bool IsPause { get; set; } = false;
+        public MainWindow MainWindow { get; set; }
+        public int maxLength { get; set; }
         public MainViewModel(MainWindow mainWindow)
         {
-
-
+            
+            MainWindow = mainWindow;
             OpenFileCommand = new RelayCommand((sender) =>
             {
                 Open.ShowDialog();
@@ -45,30 +55,74 @@ namespace CopyTextWithThreads.ViewModels
 
             CopyCommand = new RelayCommand((sender) =>
             {
+               
+            maxLength = Data.Length;
+                 thread1 = new Thread(() =>
+                  {
                 if (Data != null)
                 {
-                    for (int i = 0; i < Data.Length; i++)
-                    {
-                        Count = i;
-                       
+                     DT.Interval = TimeSpan.FromSeconds(1);
+                          DT.Tick += DT_Tick;
+                          DT.Start();
+                          for (int i = 0; i < Data.Length;i++)
+                          {
+                              Count = i;
+                            CopyData += Data[i];
+                            File.WriteAllText(Open.FileName, CopyData);  
 
-
-
-                    }
+                          }
+                    
                 }
+                 });
+
+                thread1.Start();
             });
 
 
+            PauseCommand = new RelayCommand((sender) =>
+            {
+                try
+                {
+                    thread1.Suspend();
+                    DT.Stop();
+                    isSuspend = true;                  
+                }
+                catch (Exception)
+                {                   
+                }
+                
+            });
 
+
+            ResumeCommand = new RelayCommand((sender) =>
+            {
+                try
+                {
+                    thread1.Resume();
+                    DT.Start();
+                    isSuspend = false;
+                }
+                catch (Exception)
+                {                    
+                }
+                
+            });
 
         }
 
-        public void Copy()
+        private void DT_Tick(object sender, EventArgs e)
         {
-            CopyData += Data[Count];
-            File.WriteAllText(Open.FileName, CopyData);
+            if (!isSuspend)
+            {
+                MainWindow.Progress.Maximum = maxLength;
+                MainWindow.Progress.Value += Count;
 
+                //double value = maxLength;
+                //Duration duration = new Duration(TimeSpan.FromSeconds(15));
+                //DoubleAnimation doubleanimation = new DoubleAnimation(value, duration);
+                //MainWindow.Progress.BeginAnimation(ProgressBar.ValueProperty, doubleanimation);
+            }
+           
         }
-
     }
 }
